0c33de84e3ce1527ccf532e36a3abbedd9dfebc5
basehappy19 <basehappy19@pakpoom.myself@gmail.com>
2025-09-14 23:44:34 +0700

    edit size limit

--- server/src/plugins/mulitpart.ts
+++ server/src/plugins/mulitpart.ts
@@ -3,7 +3,7 @@ import fp from "fastify-plugin";
 export default fp(async function (fastify) {
     await fastify.register(require("@fastify/multipart"), {
         limits: {
-            fileSize: 10 * 1024 * 1024, // 10MB
+            fileSize: 100 * 1024 * 1024,
             files: 1,
             fields: 10,
         },
